6b8b5e8810f53c0fad20313ae7f2e33673f18ab4
IamGauravS <ritzgaurav96@gmail.com>
2024-04-23 23:27:01 +0530

    children sum

---
+++ trees-2/l29-children-sum-proprty.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def childrenSum(self, root, node):
+        if root == None:
+            return root
+
+        child = 0
+
+        if root.left:
+            child += root.left.val
+        if root.right:
+            child += root.right.val
+
+
+        if child >= root.val:
+            root.val = child 
+
+        else:
+            if root.left:
+                root.left.val = root.val
+            if root.rightL
+                root.right.val = root.val
+
+
+        self.changeTree(root.left)
+        self.changeTree(root.right)
+
+        total = 0
+
+        if root.left:
+            tot+= root.left.val
+        if root.right:
+            tot+= root.right.val
+
+
+        if root.left or root.right:
+            root.val = tot
+
+        return
