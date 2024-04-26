15942316671b5c23d0205b81c39e6ed5c5de89d3
IamGauravS <ritzgaurav96@gmail.com>
2024-04-26 18:01:19 +0530

    more optimised solution

--- daily-challange/1289-minimum-falling-path.py
+++ daily-challange/1289-minimum-falling-path.py
@@ -21,3 +21,37 @@ class Solution:
 
 
         return min(self.dp[0]) 
+    
+
+
+## more optimsied solution
+
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+
+        self.width = len(grid[0])
+        self.height = len(grid)
+        self.dp = [[float('inf') for i in range(self.width)] for j in range(self.height)]
+
+        for i in range(self.width):
+            self.dp[-1][i] = self.grid[-1][i]
+
+        for i in range(self.height-2, -1, -1):
+            min_val, second_min_val = float('inf'), float('inf')
+            min_index = -1
+            for j in range(self.width):
+                if self.dp[i+1][j] < min_val:
+                    second_min_val = min_val
+                    min_val = self.dp[i+1][j]
+                    min_index = j
+                elif self.dp[i+1][j] < second_min_val:
+                    second_min_val = self.dp[i+1][j]
+
+            for j in range(self.width):
+                if j == min_index:
+                    self.dp[i][j] = second_min_val + self.grid[i][j]
+                else:
+                    self.dp[i][j] = min_val + self.grid[i][j]
+
+        return min(self.dp[0])
