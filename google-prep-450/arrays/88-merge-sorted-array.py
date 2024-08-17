ffec617bd48ed134ac07dc833b4aff13a94595ff
IamGauravS <iamgauravs15@gmail.com>
2024-08-17 22:13:45 +0530

    feat: Merge sorted arrays in Solution class

--- google-prep-450/arrays/88-merge-sorted-array.py
+++ google-prep-450/arrays/88-merge-sorted-array.py
@@ -3,4 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+        ## fill the first array with the numbers from second array
+        ptr1, ptr2, arrayPointer = m-1, n-1, m + n - 1
+
+        while ptr2 >= 0:
+            if ptr1 >= 0 and nums1[ptr1] > nums2[ptr2]:
+                nums1[arrayPointer] = nums1[ptr1]
+                ptr1 -= 1
+
+            else:
+                nums1[arrayPointer] = nums2[ptr2]
+                ptr2 -= 1
+
+            arrayPointer -= 1
