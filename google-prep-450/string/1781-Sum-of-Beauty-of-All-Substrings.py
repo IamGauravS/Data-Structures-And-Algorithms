8d5c4339831d7aff9d846a48b415d0eb560a5bae
IamGauravS <iamgauravs15@gmail.com>
2024-10-13 13:16:43 +0530

    refactor: Optimize getFreq function in Solution class

--- google-prep-450/string/1781-Sum-of-Beauty-of-All-Substrings.py
+++ google-prep-450/string/1781-Sum-of-Beauty-of-All-Substrings.py
@@ -1,19 +1,17 @@
 from collections import defaultdict
 class Solution:
-   
-    
+    def getFreq(self, hashMap):
+        values = hashMap.values()
+        return min(values), max(values)
     def beautySum(self, s: str) -> int:
         outputSum = 0
         for i in range(len(s)):
             hashMap = defaultdict(int)
-            minfreq, maxfreq = float('inf'), float('-inf')
             for j in range(i, len(s)):
-               
+                
                 hashMap[s[j]] += 1
-                minfreq = min(minfreq, hashMap[s[j]])
-                maxfreq = max(maxfreq, hashMap[s[j]])
+                minfreq, maxfreq = self.getFreq(hashMap)
                 outputSum += (maxfreq - minfreq)
 
         return outputSum
                 
-
