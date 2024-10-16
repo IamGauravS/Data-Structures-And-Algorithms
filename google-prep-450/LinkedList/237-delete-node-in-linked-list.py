0ad9ab506914c6a942feb31e6c0e5feabadc38a8
IamGauravS <iamgauravs15@gmail.com>
2024-10-16 17:56:09 +0530

    feat: Add deleteNode and middleNode solutions for linked list problems

---
+++ google-prep-450/LinkedList/237-delete-node-in-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        curr = node
+        nextNode = curr.next
+
+        curr.val = nextNode.val
+        curr.next = nextNode.next
+        del nextNode
+        return
