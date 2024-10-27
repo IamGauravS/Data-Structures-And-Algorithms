e0aba04c5c24aaea8fbdec527c8530622689dc36
IamGauravS <iamgauravs15@gmail.com>
2024-10-27 14:01:25 +0530

    feat: Implement binary tree level order traversal, binary tree postorder traversal, and binary tree inorder traversal
    This commit adds three new files: `102.binary-tree-level-order-traversal.py`, `145.binary-tree-postorder-traversal.py`, and `94.binary-tree-inorder-traversal.py`. These files contain the implementation of level order traversal, postorder traversal, and inorder traversal functions for a binary tree. Each function takes in a binary tree node and returns a list of node values in the specified traversal order. The implementations use recursive helper functions to traverse the tree and append the node values to the output list.
    
    Ref: #789

---
+++ google-prep-450/binary-tree/94.binary-tree-inorder-traversal.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+
+        def inorderTraversalHelper(node):
+            if node is None:
+                return
+            
+            inorderTraversalHelper(node.left)
+            output.append(node.val)
+            inorderTraversalHelper(node.right)
+
+        inorderTraversalHelper(root)
+        return output
+# @lc code=end
+
